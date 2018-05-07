3aebdb07936c8c9eefdbf0a833cf970ddef0b08e
jonataasgoomes <jonataasgoomes@gmail.com>
2018-05-07 19:30:28 -0300

    initiating authentication

--- app/controllers/site/home_controller.rb
+++ app/controllers/site/home_controller.rb
@@ -1,4 +1,5 @@
 class Site::HomeController < ApplicationController
+	before_action :authenticate_user!
 	layout "site"
   def index
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   namespace :site do
-    get 'home/index'
+    get 'home', to: 'home#index'
   end
   devise_for :users
+
   root 'site/home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
