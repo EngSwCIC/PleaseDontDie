8b5334f401a09760037b371557c89e002a46937a
jonataasgoomes <jonataasgoomes@gmail.com>
2018-05-07 19:05:01 -0300

    bootstrap and jquery install , namespaces site add

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  namespace :site do
+    get 'home/index'
+  end
   devise_for :users
-  get 'home/index'
-  root 'home#index'
+  root 'site/home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
