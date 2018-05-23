e26ec7cee136ea41c419ea9bdcd012fcd530aacd
Juliana Hosoume <ju.hosoume@gmail.com>
2018-05-23 19:59:37 -0300

    Hotfix show group

--- db/schema.rb
+++ db/schema.rb
@@ -77,7 +77,6 @@ ActiveRecord::Schema.define(version: 2018_05_23_162938) do
     t.datetime "updated_at", null: false
     t.string "first_name"
     t.string "last_name"
-    t.date "birthday"
     t.text "address"
     t.string "phone"
     t.bigint "group_id"
