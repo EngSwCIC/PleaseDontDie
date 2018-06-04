ec790a968d193961045a886460587bb867928d73
Juliana Hosoume <ju.hosoume@gmail.com>
2018-06-04 18:09:47 -0300

    Fix pet name

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,4 +1,16 @@
 class Pet < ApplicationRecord
   belongs_to :group
   belongs_to :specie, optional: true
+
+  def sex
+    sex_prettyp(self[:sex]) || self[:sex]
+  end
+
+  private
+
+  def sex_prettyp(num)
+    sex = {"1" => "male", "2" => "female"}
+    sex[num]
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_04_021840) do
+ActiveRecord::Schema.define(version: 2018_06_04_205833) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2018_06_04_021840) do
     t.boolean "done"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "until"
     t.index ["need_id"], name: "index_duties_on_need_id"
     t.index ["pet_id"], name: "index_duties_on_pet_id"
   end
