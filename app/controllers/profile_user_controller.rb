43497daf5c244d663b06308f33cd6e36a7749e98
Jonatas G. P. Junior <jonataasgoomes@gmail.com>
2018-07-02 10:18:44 -0300

    fix feed join

--- app/controllers/profile_user_controller.rb
+++ app/controllers/profile_user_controller.rb
@@ -33,10 +33,11 @@ class ProfileUserController < ApplicationController
     @duties = @user.duties.order(updated_at: :desc)
     @groups = @user.groups.order(updated_at: :desc)
     @pets = @user.pets.order(updated_at: :desc)
-    @groupsUsers = GroupsProfileUser.joins("INNER JOIN groups ON groups.id = groups_profile_users.group_id
-                                            INNER JOIN profile_users ON profile_users.id = groups_profile_users.profile_user_id")
-                                    .select("groups_profile_users.*, groups.name, profile_users.first_name")
-                                    .order(created_at: :desc)
+    @groupsUsers = Group.joins("INNER JOIN groups_profile_users ON groups_profile_users.group_id = groups.id
+                                INNER JOIN profile_users ON profile_users.id = groups_profile_users.profile_user_id")
+                        .where(groups: { id: @groups })
+                        .select("groups_profile_users.*, groups.name, profile_users.first_name")
+                        .order(created_at: :desc)
   end
   
   def friends
