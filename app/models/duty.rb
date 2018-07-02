280babf304d503bf30ef6c51121384b7a0775137
Juliana Hosoume <ju.hosoume@gmail.com>
2018-07-02 03:05:20 -0300

    mend

--- app/models/duty.rb
+++ app/models/duty.rb
@@ -1,4 +1,14 @@
 class Duty < ApplicationRecord
   belongs_to :pet
   belongs_to :need, optional: true
+
+  after_save :dup_duty
+
+  def dup_duty
+    if self.done == true
+      byebug
+      new_duty = self.dup
+      new_duty.save
+    end
+  end
 end
